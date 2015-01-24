7cb3f839e3545957b225ea73c04949533b0615d0
root <root@localhost.localdomain>
2015-01-24 09:40:54 +0200

    scan directoy of urls files

--- r4bia.py
+++ r4bia.py
@@ -37,7 +37,8 @@ class R4bia:
                    'dorks_file':"",
                    'save_dir':config.CURRENT_DIR+"/output/",
                    'dorks_country':"",
-                   'next_url':False
+                   'next_url':False,
+                    'delete_file':False,
                     }
 
         self.flags={'uflag':False,
@@ -140,7 +141,7 @@ def main(argv):
    r4bia.usage()   
 
    try:
-      opts, args = getopt.getopt(argv,"ho:f:u:s:g:p:td:a:",["next_url","goodork","add_options=","session=","dorks_file=","dorks=","demo","proxychains=","groups=","scanners=","save_dir=","file=","url=","dorks_country="])
+      opts, args = getopt.getopt(argv,"ho:f:u:s:g:p:td:a:",["delete_file","next_url","goodork","add_options=","session=","dorks_file=","dorks=","demo","proxychains=","groups=","scanners=","save_dir=","file=","url=","dorks_country="])
    except getopt.GetoptError:
       print 'scan_web.py -f <outputfile>'
       sys.exit(2)
@@ -184,6 +185,8 @@ def main(argv):
         r4bia.global_vars['goodork_flag']=True
       elif opt == "--next_url":
         r4bia.global_vars['next_url']=True
+      elif opt == "--delete_file":
+        r4bia.global_vars['delete_file']=True
       elif opt in ("-a","--add_options"):
         r4bia.global_vars['add_options_string']=arg
          
--- urlsfile.py
+++ urlsfile.py
@@ -4,6 +4,7 @@
 import uuid
 import re
 import scanurl
+import os
 
 #Start of class Scanner
 
@@ -13,26 +14,56 @@ class UrlsFile:
     def start_scan_by_file(self,global_vars,scanners,session):
         target_url=""
         line_num=1
-        with open(global_vars['targets_file']) as f:
-            for line in f:
-                if global_vars['session_name']!="":
-                    if session.session["opt"]=="load":
-                        if line_num < session.session["urls_line"]:
+        if os.path.isdir(global_vars['targets_file']):
+            for filename in os.listdir(global_vars['targets_file']):        
+                with open(global_vars['targets_file']+filename) as f:
+                    for line in f:
+                        if global_vars['session_name']!="":
+                            if session.session["opt"]=="load":
+                                if line_num < session.session["urls_line"]:
+                                    line_num=line_num+1
+                                    continue
+                            
+                            session.session["urls_line"]=line_num
+                            session.save_session()
                             line_num=line_num+1
-                            continue
-                    
-                    session.session["urls_line"]=line_num
-                    session.save_session()
-                    line_num=line_num+1
-                    print session.session["urls_line"]
+                            print session.session["urls_line"]
+                                
+                        result = re.search("(?P<url>https?://[^\s]+)", line)
+                        if result:
+                            global_vars['target_url']=result.group("url")
+                            _scanurl=scanurl.ScanUrl()
+                            _scanurl.start_scan_by_url(global_vars,scanners,session)
+                if global_vars['delete_file']:
+                    os.remove(global_vars['targets_file']+filename)
+                print 'End of urls file.:'+global_vars['targets_file']+filename
+                print str(line_num)+' urls scanned'
+                            
+        else:
+            with open(global_vars['targets_file']) as f:
+                for line in f:
+                    if global_vars['session_name']!="":
+                        if session.session["opt"]=="load":
+                            if line_num < session.session["urls_line"]:
+                                line_num=line_num+1
+                                continue
                         
-                result = re.search("(?P<url>https?://[^\s]+)", line)
-                if result:
-                    global_vars['target_url']=result.group("url")
-                    _scanurl=scanurl.ScanUrl()
-                    _scanurl.start_scan_by_url(global_vars,scanners,session)
-        print 'End of urls file.'
-        print str(line_num)+' urls scanned'
+                        session.session["urls_line"]=line_num
+                        session.save_session()
+                        line_num=line_num+1
+                        print session.session["urls_line"]
+                            
+                    result = re.search("(?P<url>https?://[^\s]+)", line)
+                    if result:
+                        global_vars['target_url']=result.group("url")
+                        _scanurl=scanurl.ScanUrl()
+                        _scanurl.start_scan_by_url(global_vars,scanners,session)
+            
+
+            if global_vars['delete_file']:
+                os.remove(global_vars['targets_file'])
+            print 'End of urls file.'
+            print str(line_num)+' urls scanned'
                     
                     
     
