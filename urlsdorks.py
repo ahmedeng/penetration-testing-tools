c4af2f80b472a02a826807aaab9c24fd00e79adc
Ahmed Hassan <ahmedh@joinvip.com>
2015-06-20 17:19:42 +0200

    fix

--- urlsdorks.py
+++ urlsdorks.py
@@ -27,10 +27,8 @@ class UrlsDorks:
         else:
             output_dir=_utils.create_save_dir(global_vars['save_dir']+"urls")
             output_filename=output_dir+str(uuid.uuid4())
-            if global_vars['dork'] or global_vars['dorks_file']:
-                print global_vars['dork']
-                global_vars["advanced_search_url"]=global_vars["advanced_search_url"].replace("[TEXT]",global_vars['dork'])
-                exit()
+            if global_vars['dorks'] or global_vars['dorks_file']:
+                global_vars["advanced_search_url"]=global_vars["advanced_search_url"].replace("[TEXT]",global_vars['dorks'])
             if global_vars["advanced_search_url"]:
                 print global_vars["advanced_search_url"]
                 u=urls.UrlGoogle('',global_vars["advanced_search_url"])
